9df1d528960ef29dbde9ce52484d6c54cdce6b61
김한별[Speech Fusion] <hanbyul.kim@navercorp.com>
2022-12-04 01:29:52 +0900

    Remove unnecessary import

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
